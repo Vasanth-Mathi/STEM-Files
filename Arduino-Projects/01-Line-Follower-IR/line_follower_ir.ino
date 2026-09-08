ad0c4d9833a0e841d980f7ef985454bc350f553e
Vasanth Mathi <vasanthkris@icloud.com>
2026-09-08 09:07:38 +0530

    Update line follower code for four-wheel chassis

--- Arduino-Projects/01-Line-Follower-IR/line_follower_ir.ino
+++ Arduino-Projects/01-Line-Follower-IR/line_follower_ir.ino
@@ -1,20 +1,27 @@
 /*
-  Line Follower Robot Using Two IR Sensors
+  4-Wheel Line Follower Robot Using Two IR Sensors
   Board: Arduino Uno
+
+  Motor arrangement:
+  - Left front + left rear motors operate together through L298N channel A.
+  - Right front + right rear motors operate together through L298N channel B.
 */
 
 const int leftIR = 2;
 const int rightIR = 3;
 
+// Left-side motor group
 const int ENA = 5;
 const int IN1 = 8;
 const int IN2 = 9;
 
+// Right-side motor group
 const int ENB = 6;
 const int IN3 = 10;
 const int IN4 = 11;
 
-const int motorSpeed = 150;
+const int forwardSpeed = 170;
+const int turnSpeed = 90;
 
 void setup() {
   pinMode(leftIR, INPUT);
@@ -35,7 +42,7 @@ void loop() {
   int leftSensor = digitalRead(leftIR);
   int rightSensor = digitalRead(rightIR);
 
-  // Assumption: LOW = black line, HIGH = white surface
+  // Assumption: LOW = black line, HIGH = light surface
   if (leftSensor == LOW && rightSensor == LOW) {
     moveForward();
   } else if (leftSensor == LOW && rightSensor == HIGH) {
@@ -48,38 +55,38 @@ void loop() {
 }
 
 void moveForward() {
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, motorSpeed);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  setLeftForward(forwardSpeed);
+  setRightForward(forwardSpeed);
 }
 
 void turnLeft() {
-  analogWrite(ENA, 80);
-  analogWrite(ENB, motorSpeed);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  setLeftForward(turnSpeed);
+  setRightForward(forwardSpeed);
 }
 
 void turnRight() {
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, 80);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+  setLeftForward(forwardSpeed);
+  setRightForward(turnSpeed);
 }
 
 void stopRobot() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void setLeftForward(int speedValue) {
+  analogWrite(ENA, speedValue);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void setRightForward(int speedValue) {
+  analogWrite(ENB, speedValue);
+  digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 }
